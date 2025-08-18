7e48de3dd31a6289c10f0eac0ce725aed9459c64
Osaretin <osaretin.frank10@gmail.com>
2025-08-18 07:14:32 +0100

    Update Next Config

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,22 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
   /* config options here */
-};
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "api.dicebear.com",
+        port: "",
+      },
+      {
+        protocol: "https",
+        hostname: "picsum.photos",
+        port: "",
+      },
+    ],
+    dangerouslyAllowSVG: true,
+  },
+}
 
-export default nextConfig;
+export default nextConfig
